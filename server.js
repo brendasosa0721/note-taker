b3557778a2096668a45799416c094935a532c784
Brenda  Sosa <brenda.sosa@gmail.com>
2022-08-25 21:51:19 -0400

    added the server js file with dependencies

---
+++ server.js
@@ -0,0 +1,24 @@
+//Dependencies
+
+const express = require('express');
+
+//Instantiate the server
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+//Listen method
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
