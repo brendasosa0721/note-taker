416b57725b7aeaaaa056d0b43bd99088bce9a242
Brenda  Sosa <brenda.sosa@gmail.com>
2022-08-26 09:18:41 -0400

    added the delete API and app.post

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT ||8000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-//Asychronous process
+//Asychronous process- Asychronous is used to return a promise
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsyn = util.promisify(fs.writeFile);
@@ -39,8 +39,30 @@ app.post("/api/notes" , function(req, res) {
         res.json(note);
     })
 })
+
+//Delete API- This is to target which note the client want to delete. Within this app.get we will go back to JSON file and this will find the notes we want to remove and add it to the newNotedate.
+
+
+app.delete("/api/notes/:id" , function(req,res) {
+    const idToDelete = parseInt(req.params.id); // creating the parameter
+    readFileAsync("./develop/db/db.json" , "utf8").then(function(data) { // using the concat method to merge the arrays below
+        const notes = [].concat(JSON.parse(data)); // Creating a new variable with the concat method
+        const newNoteData =[] //New variable for where the unwanted notes will go [] represent the array
+        for(let i = 0; i<notes.length; i++) { // For loop which is used to loop around the function
+            if(idToDelete !== notes[i].id) {
+                newNoteData.push(notes[i])
+            }
+        }
+
+        return newNoteData
+    }).then(function(notes) {
+        writeFileAsyn("./develop/db/db.json" , JSON.stringify(notes))
+        res.send('saved completed');
+    })
+})
 //Requiring data
-const { notes } = require('./')
+const { notes } = require('./');
+const { json } = require('body-parser');
 
 
 
