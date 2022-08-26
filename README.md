a9e93677c29a836da2c8cbf17156337ed15b0755
Brenda  Sosa <brenda.sosa@gmail.com>
2022-08-26 17:55:06 -0400

    rearrange app.post

--- README.md
+++ README.md
@@ -3,8 +3,21 @@
 
 # Purpose
 
+Note Taker deployed on Heroku which will allow the client to write notes, delete and save them.
+
+
 # Programs used
+
+* Node.Js
+* JavaScript
+* HTML
+* CSS
+* Express.js
+
 
 # Screenshot and live URL
 
 # Constribution
+
+Brenda Sosa
+
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const {notes} = require("./db/db.json");
+
 
 
 //Instantiate the server with express
 const app = express();
-const PORT = process.env.PORT || 8000; 
+const PORT = process.env.PORT || 3001;//Using this port it won't get stuck in the number 
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -19,24 +21,43 @@ const writeFileAsyn = util.promisify(fs.writeFile);
 
 //Middleware
 
-app.use(express.static("./develop/public"));
+app.use(express.static("public"));
 
 //API
-app.get("/api/notes", function(req, res) {
-    readFileAsync("./develop/db/db.json" , "utf8").then(function(data) {
+app.get("/api/notes", function(req, res) { // Get data
+    readFileAsync("./db/db.json" , "utf8").then(function(data) {
         res.json(notes);
     })
 })
 
-app.post("/api/notes" , function(req, res) {
-    const note = req.body;
-    readFileAsync("./develop/db/db.json" , "utf8").then(function(data) {
-        note.id = notes.length + 1
+// function createNewNote(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//         path.join(__dirname, "./db/db.json"),
+//         JSON.stringify({ notes: notesArray } , null , 2)
+        
+//     );
+//     return note;
+// }
+
+// app.post("/api/notes" , (req, res) => {
+//     const note = createNewNote(req.body, notes);
+//     res.json(req.body);
+// });
+
+
+app.post("/api/notes" , function(req, res) { // Post data
+    readFileAsync("./db/db.json" , "utf8").then(function(body) {
+        const note = req.body;
+        note.id = note.length + 1
+        console.log(note);
         note.push(note);
-        return notes
-    }).then(function(note) {
-        writeFileAsyn("./develop/db/db.json" , JSON.stringify(notes))
-        res.json(note);
+        return note
+        
+    }).then(function(notes) {
+        writeFileAsyn("./db/db.json" , JSON.stringify(note))
+        res.json(notes);
     })
 })
 
@@ -45,7 +66,7 @@ app.post("/api/notes" , function(req, res) {
 
 app.delete("/api/notes/:id" , function(req,res) {
     const idToDelete = parseInt(req.params.id); // creating the parameter
-    readFileAsync("./develop/db/db.json" , "utf8").then(function(data) { // using the concat method to merge the arrays below
+    readFileAsync("./db/db.json" , "utf8").then(function(data) { // using the concat method to merge the arrays below
         const notes = [].concat(JSON.parse(data)); // Creating a new variable with the concat method
         const newNoteData =[] //New variable for where the unwanted notes will go [] represent the array
         for(let i = 0; i<notes.length; i++) { // For loop which is used to loop around the function
@@ -56,7 +77,7 @@ app.delete("/api/notes/:id" , function(req,res) {
 
         return newNoteData
     }).then(function(notes) {
-        writeFileAsyn("./develop/db/db.json" , JSON.stringify(notes))
+        writeFileAsyn("./db/db.json" , JSON.stringify(notes))
         res.send('saved completed');
     })
 })
@@ -65,24 +86,20 @@ app.delete("/api/notes/:id" , function(req,res) {
 //HTML Routes
 
 app.get("/notes" , function(req, res) {
-    res.sendFile(path.join(__dirname, "./develop/public/index.html")); // Creating the path to connect the index.hmtl
+    res.sendFile(path.join(__dirname, "./public/notes.html")); // Creating the path to connect the index.hmtl
 });
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
-app.get("*" , function(req, res) {
-    res.sendFile(path.join(__dirname, "./develop/public/index.html")); // Adding the middleware which will be act as a bridge
-});
-
-
-
-
+// app.get("*" , function(req, res) {
+//     res.sendFile(path.join(__dirname, "./public/index.html")); // Adding the middleware which will be act as a bridge
+// });
 
 
 
 //Listen method
 
-app.listen(PORT,  function() {
-    console.log("APP listening on PORT " + PORT);
+app.listen(PORT, () => {
+    console.log(`APP listening on PORT + ${PORT}!`);
 });
